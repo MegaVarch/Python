8aea3298ad510275db2c69549f8999b3cf40a9d5
MegaVarch <ishaa.sngh@gmail.com>
2026-03-22 12:32:58 +0530

    Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,32 @@
+name = "Snake"
+age = 10
+is_student = True
+weight = 28.6
+
+print("name :", name)
+print("Data type of name is: ", type(name))
+
+print("age :", name)
+print("Data type of age is: ", type(age))
+
+print("is_student :", name)
+print("Data type of is_student is: ", type(is_student))
+
+print("weight :", name)
+print("Data type of weight is: ", type(weight))
+
+print("\n After typecasting...")
+name = str(name)
+print(name)
+
+print("\n After typecasting...")
+age = str(age)
+print(age)
+
+print("\n After typecasting...")
+is_student = str(is_student)
+print(is_student)
+
+print("\n After typecasting...")
+weight = int(weight)
+print(weight)
