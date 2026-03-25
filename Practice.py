60d6cb86fe7c2d712da5cde61dd311b97cf009dd
MegaVarch <ishaa.sngh@gmail.com>
2026-03-25 18:31:58 +0530

    Statements

---
+++ Practice.py
@@ -0,0 +1,9 @@
+age = int(input("What is your age: "))
+
+if age < 13:
+    print("You are a CHILD")
+elif age >= 13 and age < 18:
+    print("You are a TEEN")
+else:
+    print("You are an ADULT")
+
---
+++ Statements.py
@@ -0,0 +1,8 @@
+num = int(input("Enter your number: "))
+
+
+if num > 0:
+    print(num, "is a positive number")
+
+if num < 0:
+    print(num, "is a negative number")
