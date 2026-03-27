0355d30dd42c7d1674b00c01ba4188deada1e734
MegaVarch <ishaa.sngh@gmail.com>
2026-03-27 12:27:19 +0530

    Not equal

---
+++ Notequal.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+d = "python"
+e = "coding"
+
+if a != b:
+   print(a, "is different from" ,b) 
+
+a = 4
+b = 5
+
+if(a == 1) != (b==5):
+   print("Hello")
+
+a = int(input("enter a number"))
+
+if a%2 != 0:
+   print(a, "is not an even number")
