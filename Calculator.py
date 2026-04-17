1c9bf41a0feaea6e6c74988dda92c26554a0726e
MegaVarch <ishaa.sngh@gmail.com>
2026-04-17 18:39:53 +0530

    Calculating power

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+print("Enter numbers: ")
+main_num = int(input("Enter the numner to find power of: "))
+power = int(input("Enter power to add on base number: "))
+
+result = main_num ** power
+
+print("Result:", result)
