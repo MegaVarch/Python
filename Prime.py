37594a916f7997cf7bb5853227f088bc3d8114f5
MegaVarch <ishaa.sngh@gmail.com>
2026-04-25 12:51:11 +0530

    Prime

---
+++ Prime.py
@@ -0,0 +1,13 @@
+lower = int(input("Please enter a lower range: "))
+upper = int(input("Please enter a upper range: "))
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+        
