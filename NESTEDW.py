cb875260c7605d99b7b868ba1adce273e6adbbbe
MegaVarch <ishaa.sngh@gmail.com>
2026-04-25 12:36:43 +0530

    Letters

---
+++ NESTEDW.py
@@ -0,0 +1,11 @@
+string = input("Please Enter your own word: ")
+
+char = input("Please enter your own character: ")
+i = 0
+count = 0
+while (i < len(string)): 
+    if (string[i] == char):
+        count = count + 1
+    i = i + 1
+
+print("THE TOTAL NUMBER OF TIMES",char, "HAS OCCURED IS ",count)
