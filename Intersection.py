da8d829044108801f6ec7633ed6b93bf8df2ce53
MegaVarch <ishaa.sngh@gmail.com>
2026-07-04 12:40:39 +0530

    Intersection

---
+++ Intersection.py
@@ -0,0 +1,11 @@
+setx = {"blue", "green"}
+sety = {"yellow", "blue"}
+
+print("Original set elements:")
+print(setx)
+print(sety)
+
+print("\nIntersection of two said sets")
+setz = setx.intersection(sety)
+
+print(setz)
