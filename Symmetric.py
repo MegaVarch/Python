56a052a84a5a568ac580508fcf667bd694b697ad
MegaVarch <ishaa.sngh@gmail.com>
2026-07-08 17:46:16 +0530

    Symmetric

---
+++ Symmetric.py
@@ -0,0 +1,7 @@
+set1 = {'blue', 'green'}
+set2 = {'blue', 'yellow'}
+print(set1.symmetric_difference(set2))
+
+set3 = {1, 2, 3, 4, 5}
+set4 = {1, 5, 6, 7, 8, 9}
+print(set3.symmetric_difference(set4))
