32ea5ea4ed4a920a7e58e0992e6bb9a587f859fd
MegaVarch <ishaa.sngh@gmail.com>
2026-08-09 13:37:22 +0530

    Habit

---
+++ Habit.py
@@ -0,0 +1,15 @@
+habit = ("Reading", "Exercise", "Homework", "Guitar", "Coding")
+
+print("Habit tuple:", habit)
+print("Number of habits:", len(habit))
+
+print("First habit:", habit[0])
+print("Last habit:", habit[-1])
+
+print("First three habits:", habit[:3])
+print("Last two habits:", habit[-2:])
+
+print("All habits:")
+for h in habit:
+    print(h)
+
